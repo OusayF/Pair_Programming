5a5bfc2f5c17e23c84a26a6adb46db79b200ae7f
Lassmann9 <75848603+Lassmann9@users.noreply.github.com>
2020-12-13 23:40:20 +0100

    Add files via upload

---
+++ Blatt4_a.py
@@ -0,0 +1,28 @@
+'''
+This function should calculate the dice rolls
+
+'''
+__author__ = "5984035, Laßmann, XXXXXXX, XXXXXX"
+
+import random
+
+#faces_o = [1, 2, 3, 4, 5, 6]
+
+def roll_dice(number = 1, faces = 6,seed = None):
+'''
+'''
+
+    while count <= number:
+        random.randrange(1,faces + 1)
+        count += 1
+
+
+
+
+# TODO
+def player_list():
+    print("Bitte geben Sie den Spielernamen ein ")
+    players = []
+
+    # Add a Player
+    players.append([i,0])
---
+++ Blatt4_b.py
@@ -0,0 +1,9 @@
+
+__author__ = "5984035, Laßmann, XXXXXXX, XXXXXX"
+
+
+def sixteen_is_dead(player):
+
+
+
+
